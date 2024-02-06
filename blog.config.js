15ca1534c4c3f2c051c2a0487fdec824eff14dff
CATO <129149926+CATO20230328@users.noreply.github.com>
2024-02-06 10:15:15 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -160,8 +160,8 @@ const BLOG = {
   PREVIEW_CATEGORY_COUNT: 16, // 首页最多展示的分类数量，0为不限制
   PREVIEW_TAG_COUNT: 16, // 首页最多展示的标签数量，0为不限制
 
-  POST_DISABLE_GALLERY_CLICK: process.env.NEXT_PUBLIC_POST_DISABLE_GALLERY_CLICK || false, // 画册视图禁止点击，方便在友链页面的画册插入链接
-  GALLERY_BEAUTIFICATION: process.env.GALLERY_BEAUTIFICATION || false, // 画册视图美化与功能增强
+  POST_DISABLE_GALLERY_CLICK: process.env.NEXT_PUBLIC_POST_DISABLE_GALLERY_CLICK || true, // 画册视图禁止点击，方便在友链页面的画册插入链接
+  GALLERY_BEAUTIFICATION: process.env.GALLERY_BEAUTIFICATION || true, // 画册视图美化与功能增强
 
   //   ********动态特效相关********
   // 鼠标点击烟花特效
